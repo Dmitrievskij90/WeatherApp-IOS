75908b5ed384539c52697cd09eed06823c487f2f
Konstantin Dmitrievskiy <dmitrievskij90@mail.ru>
2021-05-30 22:49:38 +0600

    add textField delegate methods

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -20,14 +20,47 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchTextField.delegate = self
 
     }
     @IBAction private func locationButtonPressed(_ sender: UIButton) {
     }
     @IBAction private func searchButtonPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
     }
 
 }
 
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        searchTextField.text = ""
+
+        return true
+    }
+
+    func textFieldDidEndEditing(_ textField: UITextField) {
+
+        if let city = searchTextField.text {
+            weatherManager.fetchWeather(cityName: city)
+        }
+
+        searchTextField.text = ""
+    }
+
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Tap something"
+            DispatchQueue.main.asyncAfter(deadline: .now() + 5.0) {
+                textField.placeholder = "Search"
+            }
+            return false
+        }
+    }
+
+}
 
 
