4c32bfb20f27de49daeac79c6529b30cc6007901
Konstantin Dmitrievskiy <dmitrievskij90@mail.ru>
2021-05-30 21:59:32 +0600

    add WeatherModel

---
+++ WeatherApp/WeatherModel.swift
@@ -0,0 +1,47 @@
+//
+//  WeatherModel.swift
+//  WeatherApp
+//
+//  Created by Konstantin Dmitrievskiy on 30.05.2021.
+//
+
+import Foundation
+
+struct WeatherModel {
+    let conditionID: Int
+    let cityName: String
+    let temperature: Double
+    let date: Int
+    let humidity: Int
+    let windSpeed: Double
+    let description: String
+
+    var temperatureString: String {
+        return String(format: "%.1f", temperature)
+    }
+
+    var conditionName: String {
+        switch conditionID {
+        case 200...232:
+            return "cloud.bolt"
+        case 300...321:
+            return "cloud.drizzle"
+        case 500...531:
+            return "cloud.rain.fill"
+        case 600...622:
+            return "snow"
+        case 701...781:
+            return "cloud.fog"
+        case 800:
+            return "sun.max"
+        case 801...804:
+            return "cloud.bolt"
+        default:
+            return "cloud"
+        }
+    }
+
+}
+
+
+
