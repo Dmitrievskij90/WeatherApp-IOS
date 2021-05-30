493d0d268a281df3695d7cbb9682d3389a5aa4f3
Konstantin Dmitrievskiy <dmitrievskij90@mail.ru>
2021-05-30 22:54:59 +0600

    add custom cell

---
+++ WeatherApp/HourlyTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  HourlyTableViewCell.swift
+//  WeatherApp
+//
+//  Created by Konstantin Dmitrievskiy on 30.05.2021.
+//
+
+import UIKit
+
+class HourlyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
