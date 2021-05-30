355c2d44ae67171e7f9c94d81d2199be9b891d72
Konstantin Dmitrievskiy <dmitrievskij90@mail.ru>
2021-05-30 21:27:45 +0600

    add outlets and actions

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    @IBOutlet weak var windLabel: UILabel!
+    @IBOutlet weak var conditionImageVIew: UIImageView!
+    @IBOutlet weak var weatherTableView: UITableView!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+    }
+    @IBAction private func locationButtonPressed(_ sender: UIButton) {
+    }
+    @IBAction private func searchButtonPressed(_ sender: UIButton) {
     }
 
-
 }
+
+
 
