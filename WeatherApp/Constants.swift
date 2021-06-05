c7f0d65afd7f84d82b55865248f7c87e5574a1fc
Konstantin Dmitrievskiy <dmitrievskij90@mail.ru>
2021-06-05 12:56:59 +0600

    add Constants file and change weather images

---
+++ WeatherApp/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  WeatherApp
+//
+//  Created by Konstantin Dmitrievskiy on 05.06.2021.
+//
+
+import Foundation
+
+struct Constants {
+    static let thunderstorm = "ic_thunderstorm"
+    static let drizzle = "ic_drizzle"
+    static let rain = "ic_rain"
+    static let snow = "ic_snow"
+    static let wind = "ic_wind"
+    static let sun = "ic_sun"
+    static let cloudy = "ic_cloudy"
+    static let clouds = "ic_clouds"
+    static let dateFormat = "EEEE HH:mm"
+}
+
